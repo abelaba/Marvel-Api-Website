4d98b0616d776846c12c0705b3bb4073e5644ed3
abelaba <se.abel.abate@gmail.com>
2021-03-03 17:50:14 +0300

    works added

--- assets/css/js/index.js
+++ assets/css/js/index.js
@@ -51,3 +51,56 @@ async function loadDataFunction(offset) {
      return total;
      
   }
+
+
+  async function works(){
+
+    var total = await gettotal().then((data)=>{return data})
+    var array = await loadDataFunction(0).then((data)=>data.data.results)
+    
+    
+
+    for (let i = 0; i < array.length; i++) {
+        spanDiv.innerHTML += `${cardCreate2(array[i].name,array[i].thumbnail.path,array[i].description)}`
+        
+    }
+
+    //link creation
+    for (let i = 0; i < total; i+=100) {
+        var link = document.createElement("a")
+        link.className = `${i/100}`
+        link.innerText = `${i/100}`
+        link.setAttribute("href","javascript:void(0)")
+        spanLinks.appendChild(link)    
+    }
+
+   
+    
+    
+    document.querySelectorAll(".links a").forEach((item)=>{
+        item.addEventListener('click',()=>{
+            console.log(parseInt(item.className)*100);
+            
+            async function inner(){
+                var z =''
+                var myarray = await loadDataFunction(parseInt(item.className)*100).then((data)=>data.data.results)
+                for (let i = 0; i < myarray.length; i++) {
+                
+                    z += `${cardCreate2(myarray[i].name,myarray[i].thumbnail.path,array[i].description)}`
+                    
+                }
+                 spanDiv.innerHTML = z
+                 console.log(myarray);
+                //location.reload()
+            }
+            inner()  
+    
+        })
+    
+    })
+    allLi = document.querySelectorAll("h1");
+  
+
+}
+
+works()
