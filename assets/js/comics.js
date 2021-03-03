4bbcc0475a14026b7758dff9c4292852b4c8dd0a
ruthzaid <se.ruth.zaid@gmail.com>
2021-03-03 20:11:26 +0300

    getting variables from html file

--- assets/js/comics.js
+++ assets/js/comics.js
@@ -46,3 +46,26 @@ async function loadDataFunction(offset) {
      return data;
      
   }
+
+  async function gettotal() {
+
+    var publickey = "4009707bfdf8f467b3dc12d8d2e25bb7"
+    var privatekey = "be49e4a5b695e93c069c6e3ae89496e5741366f3"
+
+    //open the request
+
+     let response = await fetch(`https://gateway.marvel.com:443/v1/public/comics?hash=0b5bbd635d0487398a2199c5417c0969&ts=1&apikey=4009707bfdf8f467b3dc12d8d2e25bb7`);
+      
+     let data = await response.json(); 
+
+     let total =await data.data.total
+
+     
+       
+     return total;
+     
+  }
+
+var spanDiv = document.querySelector(".body")
+var spanLinks = document.querySelector(".links")
+var allLi;
