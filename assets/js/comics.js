4d815c5799bb7b4bac465dc1f934828cd236a6a8
ruthzaid <se.ruth.zaid@gmail.com>
2021-03-03 20:07:49 +0300

    loading data function

--- assets/js/comics.js
+++ assets/js/comics.js
@@ -29,3 +29,20 @@ function cardCreate2(title,image,description){
     </div>`
     
 }
+
+async function loadDataFunction(offset) {
+
+    var publickey = "4009707bfdf8f467b3dc12d8d2e25bb7"
+    var privatekey = "be49e4a5b695e93c069c6e3ae89496e5741366f3"
+
+    //open the request
+
+     let response = await fetch(`https://gateway.marvel.com:443/v1/public/comics?hash=0b5bbd635d0487398a2199c5417c0969&ts=1&apikey=4009707bfdf8f467b3dc12d8d2e25bb7&limit=100&offset=${offset}`);
+      
+     let data = await response.json(); 
+
+     total = data.data.total
+       
+     return data;
+     
+  }
